b8f0a85f7042e5c41b0cc15198beaac34aeb10fe
Daniel Ivanisevic <divanise@mac.com>
2025-08-05 15:44:44 -0400

    Convert the Java landing page to a template.

--- hello-java/src/main/java/hello/App.java
+++ hello-java/src/main/java/hello/App.java
@@ -1,15 +1,34 @@
 package hello;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.config.Configurator;
+
+import com.hubspot.jinjava.Jinjava;
 
 import io.javalin.Javalin;
 
 public class App {
 
     private static final Logger LOGGER = LogManager.getLogger();
+
+    private static final ConcurrentMap<String, String> TEMPLATES = new ConcurrentHashMap<>();
+    private static final Jinjava JINJAVA;
+
+    static {
+        JINJAVA = new Jinjava();
+        JINJAVA.setResourceLocator((fullName, charset, interperter) -> {
+            return locateTemplate(fullName);
+        });
+    }
 
     public static void main(String[] args) throws Exception {
         var config = Config.load();
@@ -25,11 +44,29 @@ public class App {
 
                     javalinConfig.jetty.defaultHost = config.httpHost();
                     javalinConfig.jetty.defaultPort = config.httpPort();
+
+                    javalinConfig.fileRenderer((filePath, model, ctx) -> {
+                        return JINJAVA.render(locateTemplate(filePath), model);
+                    });
                 })
                 .get("/", ctx -> {
-                    var loader = ClassLoader.getSystemResourceAsStream("html/index.html");
-                    ctx.writeSeekableStream(loader, "text/html; charset=utf-8");
+                    var name = Objects.requireNonNullElse(ctx.queryParam("name"), "World");
+
+                    ctx.header("content-type", "text/html; charset=utf-8");
+                    ctx.render("greeter.html", Map.of("name", name));
                 })
                 .start();
+    }
+
+    private static String locateTemplate(String name) {
+        return TEMPLATES.computeIfAbsent(name, (key) -> {
+            try {
+                var resourceName = "templates/" + key;
+                var stream = ClassLoader.getSystemResourceAsStream(resourceName);
+                return new String(stream.readAllBytes(), StandardCharsets.UTF_8);
+            } catch (IOException | RuntimeException e) {
+                throw new RuntimeException("Loading template '" + key + "' failed", e);
+            }
+        });
     }
 }
