423935342a7cb4d52b8a25f47e94d9cdf5fddf21
Daniel Ivanisevic <divanise@mac.com>
2025-08-05 13:34:16 -0400

    Convert the Rust landing page to a template.

--- hello-rs/src/routes/mod.rs
+++ hello-rs/src/routes/mod.rs
@@ -1,14 +1,13 @@
 use axum::Router;
-use axum::response::Html;
 use axum::routing::get;
 use tower_http::cors::CorsLayer;
 use tower_http::trace::TraceLayer;
 
-const INDEX_HTML: &str = include_str!("index.html");
+mod greeter;
 
 pub fn new() -> Router {
     Router::new()
-        .route("/", get(Html(INDEX_HTML)))
+        .route("/", get(greeter::greeter))
         .layer(TraceLayer::new_for_http())
         .layer(CorsLayer::very_permissive())
 }
